cbe453912bbe22fc26696209fe1cce72abeb9731
Sinan Chaush <sinansonmez@outlook.com>
2021-11-11 12:07:19 +0100

    Register Error Handling

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,21 +1,15 @@
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '../theme'
 import { createClient, Provider } from "urql";
 
 const client = createClient({url: "http://localhost:4000/graphql"})
 
-function MyApp({Component, pageProps}) {
+function MyApp({Component, pageProps}: any) {
   return (
     <Provider value={client}>
       <ChakraProvider resetCSS theme={theme}>
-        <ColorModeProvider
-          options={{
-            useSystemColorMode: true,
-          }}
-        >
-          <Component {...pageProps} />
-        </ColorModeProvider>
+        <Component {...pageProps} />
       </ChakraProvider>
     </Provider>
   )
--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -4,18 +4,27 @@ import Wrapper from "../components/Wrapper";
 import { Box, Button } from "@chakra-ui/react";
 import InputField from "../components/InputField";
 import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
 
 interface OwnProps {
 }
 
 const Register: FunctionComponent<OwnProps> = ({}) => {
   const [, register] = useRegisterMutation()
+  const router = useRouter()
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{username: "", password: ""}}
-        onSubmit={async (values) => {
+        onSubmit={async (values, {setErrors}) => {
           const response = await register(values);
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            // worked
+            router.push("/");
+          }
         }}
       >
         {({isSubmitting}) => (
