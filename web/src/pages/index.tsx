3e246369c346dede08fa079d31c07224e882d80f
Sinan Chaush <sinansonmez@outlook.com>
2021-11-17 14:19:55 +0100

    Index page is updated to show all posts

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,11 +1,19 @@
-import NavBar from "../components/NavBar";
-import { withUrqlClient } from "next-urql";
-import { createUrqlClient } from "../utils/createUrqlClient";
+import {withUrqlClient} from "next-urql";
+import {createUrqlClient} from "../utils/createUrqlClient";
+import {usePostsQuery} from "../generated/graphql";
+import Layout from "../components/Layout";
 
-const Index = () => (
-  <div>
-    <NavBar/>
-  </div>
-)
+const Index = () => {
+  const [response] = usePostsQuery()
+  return (
+    <Layout>
+      {!response.data ? (
+        <div>loading...</div>
+      ) : (
+        response.data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+      )}
+    </Layout>
+  )
+}
 
 export default withUrqlClient(createUrqlClient, {ssr: true})(Index)
