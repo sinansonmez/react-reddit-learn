1219a975a98f6e3e3266e0c50d71356ca7ad4f52
Sinan Chaush <sinansonmez@outlook.com>
2021-11-17 14:21:47 +0100

    Index page is updated to show all posts

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -5,6 +5,7 @@ import Layout from "../components/Layout";
 
 const Index = () => {
   const [response] = usePostsQuery()
+  console.log(response)
   return (
     <Layout>
       {!response.data ? (
