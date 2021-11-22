f60eae082d065048f1596d00195576e1ab66384d
Sinan Chaush <sinansonmez@outlook.com>
2021-11-22 23:47:33 +0100

    Cache vote with fragment

--- web/src/utils/createUrqlClient.ts
+++ web/src/utils/createUrqlClient.ts
@@ -1,9 +1,17 @@
 import {dedupExchange, fetchExchange} from "urql";
 import {cacheExchange, Resolver} from "@urql/exchange-graphcache";
-import {LoginMutation, LogoutMutation, MeDocument, MeQuery, RegisterMutation,} from "../generated/graphql";
+import {
+  LoginMutation,
+  LogoutMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+  VoteMutationVariables,
+} from "../generated/graphql";
 import betterUpdateQuery from "./betterUpdateQuery";
 import {pipe, tap} from "wonka";
 import Router from "next/router";
+import gql from "graphql-tag";
 
 // @ts-ignore
 const errorExchange = ({forward}) => (ops$: any) => {
@@ -72,6 +80,30 @@ export const createUrqlClient = (ssrExchange: any) => ({
       },
       updates: {
         Mutation: {
+          vote: (_result, args, cache, _info) => {
+            const {postId, value} = args as VoteMutationVariables;
+            const data = cache.readFragment(
+              gql`
+                  fragment _ on Post {
+                      id
+                      points
+                      voteStatus
+                  }
+              `, {id: postId} as any
+            );
+            if (data) {
+              if (data.voteStatus === value) return;
+              const newPoints = (data.points as number) + (!data.voteStatus ? 1 : 2) * value;
+                cache.writeFragment(
+                  gql`
+                      fragment __ on Post {
+                          points
+                          voteStatus
+                      }
+                  `, { id: postId, points: newPoints, voteStatus: value } as any
+                );
+            }
+          },
           createPost: (_result, _args, cache, _info) => {
             const allFieds = cache.inspectFields("Query")
             const fieldInfos = allFieds.filter((info) => info.fieldName === "posts")
